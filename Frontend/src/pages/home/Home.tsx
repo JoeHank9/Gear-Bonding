b22b75bdb1e07468bdc79ea014c566590ad3097a
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-05-21 14:13:03 -0600

    Frontend created

---
+++ Frontend/src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+
+
+function Home() {
+ 
+
+  return (
+    <h1>Estas en Home</h1>
+  )
+}
+
+export { Home };
---
+++ Frontend/src/pages/main/Main.tsx
@@ -0,0 +1,5 @@
+function Main() {
+  return <h1>Hola Mundo estas en MAIN</h1>;
+}
+
+export { Main };
