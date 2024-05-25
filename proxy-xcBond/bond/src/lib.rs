c312ced60c8256b612c92e8dd091e814deddc2c6
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-05-24 20:18:34 -0600

    io bond init

--- proxy-xcBond/bond/src/lib.rs
+++ proxy-xcBond/bond/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+
+use gstd::{prelude::*,msg, ActorId, MessageId};
+
+
+static mut BOND: Option<Bond> = None;
+
+struct Bond{
+    bonding_program_id: ActorId, // smart program del bonding variable y muliple para muchos proyectos
+    ft_token: ActorId, //Fungible Token for bond purchase
+    bond_token: ActorId, //Bond token of the project
+    price: u128, //price of the token
+    amount: u128, //amount of the token
+    vesting_period: u64, //vesting period of the token
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
